01089bfbe1058d070415cac0186b4ed274b4c591
599680041 <34378330+599680041@users.noreply.github.com>
2018-11-06 20:19:21 +0800

    Update init_config.sh
    Initialization configuration file

--- init_config.sh
+++ init_config.sh
@@ -37,7 +37,7 @@ uname -a
  
 # 确认内核高于4.1后，开启IPVS
 cat > /etc/sysconfig/modules/ipvs.modules <<EOF
- 
+
 #!/bin/bash
 ipvs_modules="ip_vs ip_vs_lc ip_vs_wlc ip_vs_rr ip_vs_wrr ip_vs_lblc ip_vs_lblcr ip_vs_dh ip_vs_sh ip_vs_fo ip_vs_nq ip_vs_sed ip_vs_ftp nf_conntrack_ipv4"
 for kernel_module in \${ipvs_modules}; do
